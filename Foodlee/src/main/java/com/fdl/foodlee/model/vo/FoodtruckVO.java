df3e1180bd1c74f9aab616475df749efca16ed9a
jsr8091 <Answer@DESKTOP-THU229L>
2020-05-04 17:00:13 +0900

    aaaa

--- Foodlee/src/main/java/com/fdl/foodlee/model/vo/FoodtruckVO.java
+++ Foodlee/src/main/java/com/fdl/foodlee/model/vo/FoodtruckVO.java
@@ -1,5 +1,18 @@
 package com.fdl.foodlee.model.vo;
 
-public class FoodtruckVO {
+import java.sql.Timestamp;
 
+public class FoodtruckVO {
+	
+	private int sellerId; // 판매자 번호 ⇔ integer seller_id NN <<FK>>
+	private String foodtruckImgPath; // 푸드트럭 이미지 경로 ⇔ varchar (256) foodtruck_img_path  NN
+	private String foodtruckName; // 푸드트럭 이름 ⇔ varchar (64) foodtruck_name NN
+	private String foodtruckMainMenu; // 푸드트럭 대표메뉴 ⇔ varchar (512) foodtruck_main_menu NN
+	private String foodtruckLocation; // 푸드트럭 위치 ⇔ varchar(256) foodtruck_location NN
+	private String foodtruckOperationHour; // 푸드트럭 영업시간 ⇔ varchar (64)  foodtruck_operation_hour NN	
+	private int favoriteCount; // 좋아요 트럭 ⇔ integer favorite_count <<FK>>
+	private String sellerFoodtruckCoordinate; // 판매자 푸드트럭 좌표 ⇔ varchar(1024) seller_foodtruck_coordinate NN
+	private Timestamp locationUpdatedAt; // 위치이동날짜 ⇔ timestamp location_updated_at CURRENT_TIMESTAMP
+	
+	
 }
