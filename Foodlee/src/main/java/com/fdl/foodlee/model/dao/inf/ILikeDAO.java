82c78916355af8d5648727b04a0b368aa9f377d2
compass-121 <58839593+compass-121@users.noreply.github.com>
2020-05-06 15:19:09 +0900

    v

---
+++ Foodlee/src/main/java/com/fdl/foodlee/model/dao/inf/ILikeDAO.java
@@ -0,0 +1,7 @@
+package com.fdl.foodlee.model.dao.inf;
+
+import com.fdl.foodlee.model.vo.LikeVO;
+
+public class ILikeDAO {
+	
+}
--- Foodlee/src/main/java/com/fdl/foodlee/model/vo/LikeVO.java
+++ Foodlee/src/main/java/com/fdl/foodlee/model/vo/LikeVO.java
@@ -1,9 +1,44 @@
 package com.fdl.foodlee.model.vo;
 
 public class LikeVO {
-	private MenuVO menuPic;
-	private MenuVO menuName;
-	private MenuVO menuPrice;
+//	private MenuVO menuPic; 
+//	private MenuVO menuName;
+//	private MenuVO menuPrice;
 	private int likeMenuNum; //메뉴 좋아요 개수
 	private int likefoodtruckNum; //푸드트럭 좋아요 개수
+	
+	public LikeVO() {}
+	
+	public LikeVO(int likeMenuNum) {
+		this.likeMenuNum = likeMenuNum;
+	}
+	
+	public LikeVO(int likeMenuNum, int likefoodtruckNum) {
+		super();
+		this.likeMenuNum = likeMenuNum;
+		this.likefoodtruckNum = likefoodtruckNum;
+	}
+
+	public int getLikeMenuNum() {
+		return likeMenuNum;
+	}
+
+	public void setLikeMenuNum(int likeMenuNum) {
+		this.likeMenuNum = likeMenuNum;
+	}
+
+	public int getLikefoodtruckNum() {
+		return likefoodtruckNum;
+	}
+
+	public void setLikefoodtruckNum(int likefoodtruckNum) {
+		this.likefoodtruckNum = likefoodtruckNum;
+	}
+
+	@Override
+	public String toString() {
+		return "LikeVO [likeMenuNum=" + likeMenuNum + ", likefoodtruckNum=" + likefoodtruckNum + "]";
+	}
+	
+	
 }
