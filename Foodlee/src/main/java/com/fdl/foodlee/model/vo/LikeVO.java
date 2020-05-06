75f7e0fb131e7cc772119ad2027ab321177171ae
compass-121 <58839593+compass-121@users.noreply.github.com>
2020-05-06 17:38:07 +0900

    v

--- Foodlee/src/main/java/com/fdl/foodlee/model/vo/LikeVO.java
+++ Foodlee/src/main/java/com/fdl/foodlee/model/vo/LikeVO.java
@@ -1,10 +1,11 @@
 package com.fdl.foodlee.model.vo;
 
 public class LikeVO {
-	private MenuVO menuPic; // 메뉴 사진
-	private FoodtruckVO foodtruckName; // 푸드트럭 이름
-	private MenuVO menuName; // 메뉴 이름
-	private MenuVO menuPrice; // 메뉴 가격
+//	private MenuVO menuPic; // 메뉴 사진
+//	private FoodtruckVO foodtruckName; // 푸드트럭 이름
+//	private MenuVO menuName; // 메뉴 이름
+//	private MenuVO menuPrice; // 메뉴 가격
+	
 	
 	private int likeMenuNum; //메뉴 좋아요 개수
 	private int likefoodtruckNum; //푸드트럭 좋아요 개수
