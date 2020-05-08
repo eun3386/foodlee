6299802ccbcf450d94088320b9dfb586bd2eb91d
compass-121 <58839593+compass-121@users.noreply.github.com>
2020-05-08 14:19:27 +0900

    da

--- Foodlee/src/main/java/com/fdl/foodlee/model/vo/LikeVO.java
+++ Foodlee/src/main/java/com/fdl/foodlee/model/vo/LikeVO.java
@@ -8,20 +8,29 @@ public class LikeVO {
 	
 	private int likeNum; // 좋아요 번호
 	private int likeCount; // 좋아요 수
+	private String likeMenuPic; // 좋아요 한 메뉴 사진
+	private String likeFoodtruckName; // 좋아요 한 푸드트럭 이름
+	private String likeMenuName; // 좋아요 한 메뉴 이름
+	private int likeMenuPrice; // 좋아요 한 메뉴 가격
+	
 	private int likeMenuNum; //메뉴 좋아요 개수
 	private int likefoodtruckNum; //푸드트럭 좋아요 개수
 	
 	public LikeVO() {}
 	
-	public LikeVO(int likeNum, int likeCount) {
-		this.likeNum = likeNum;
-		this.likeCount = likeCount;
+	public LikeVO(String likeMenuPic, String likeFoodtruckName, String likeMenuName, int likeMenuPrice) {
+		this(0, 0, likeMenuPic, likeFoodtruckName, likeMenuName, likeMenuPrice, 0, 0);
 	}
-	
-	public LikeVO(int likeNum, int likeCount, int likeMenuNum, int likefoodtruckNum) {
+
+	public LikeVO(int likeNum, int likeCount, String likeMenuPic, String likeFoodtruckName, String likeMenuName,
+			int likeMenuPrice, int likeMenuNum, int likefoodtruckNum) {
 		super();
 		this.likeNum = likeNum;
 		this.likeCount = likeCount;
+		this.likeMenuPic = likeMenuPic;
+		this.likeFoodtruckName = likeFoodtruckName;
+		this.likeMenuName = likeMenuName;
+		this.likeMenuPrice = likeMenuPrice;
 		this.likeMenuNum = likeMenuNum;
 		this.likefoodtruckNum = likefoodtruckNum;
 	}
@@ -42,6 +51,38 @@ public class LikeVO {
 		this.likeCount = likeCount;
 	}
 
+	public String getLikeMenuPic() {
+		return likeMenuPic;
+	}
+
+	public void setLikeMenuPic(String likeMenuPic) {
+		this.likeMenuPic = likeMenuPic;
+	}
+
+	public String getLikeFoodtruckName() {
+		return likeFoodtruckName;
+	}
+
+	public void setLikeFoodtruckName(String likeFoodtruckName) {
+		this.likeFoodtruckName = likeFoodtruckName;
+	}
+
+	public String getLikeMenuName() {
+		return likeMenuName;
+	}
+
+	public void setLikeMenuName(String likeMenuName) {
+		this.likeMenuName = likeMenuName;
+	}
+
+	public int getLikeMenuPrice() {
+		return likeMenuPrice;
+	}
+
+	public void setLikeMenuPrice(int likeMenuPrice) {
+		this.likeMenuPrice = likeMenuPrice;
+	}
+
 	public int getLikeMenuNum() {
 		return likeMenuNum;
 	}
@@ -60,11 +101,12 @@ public class LikeVO {
 
 	@Override
 	public String toString() {
-		return "LikeVO [likeNum=" + likeNum + ", likeCount=" + likeCount + ", likeMenuNum=" + likeMenuNum
-				+ ", likefoodtruckNum=" + likefoodtruckNum + "]";
+		return "LikeVO [likeNum=" + likeNum + ", likeCount=" + likeCount + ", likeMenuPic=" + likeMenuPic
+				+ ", likeFoodtruckName=" + likeFoodtruckName + ", likeMenuName=" + likeMenuName + ", likeMenuPrice="
+				+ likeMenuPrice + ", likeMenuNum=" + likeMenuNum + ", likefoodtruckNum=" + likefoodtruckNum + "]";
 	}
 	
-
+	
 	
 	
 }
