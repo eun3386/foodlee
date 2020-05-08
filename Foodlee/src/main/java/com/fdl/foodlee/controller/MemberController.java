b2c2c0622f3b65de05ec81aa38d16ff9fb8f20c6
bangbyb <58839366+bangbyb@users.noreply.github.com>
2020-05-08 17:33:14 +0900

    5.8 윤찬규

--- Foodlee/src/main/java/com/fdl/foodlee/controller/MemberController.java
+++ Foodlee/src/main/java/com/fdl/foodlee/controller/MemberController.java
@@ -4,6 +4,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/*
+- 회원가입구분/약관동의 할 수 있다.
+	member/join_choice_form.fdl (get, )
+- 회원가입 할 수 있다.
+	member/new_join_form.fdl
+	member/join.fdl
+- 아이디를 찾을 수 있다.
+	member/find_id.fdl
+- 비밀번호를 찾을 수 있다.
+	member/find_pw.fdl
+- 아이디 중복체크를 할 수 있다.
+	member/dupcheck.fdl
+- 로그인 할 수 있다.
+	member/login_form.fdl
+	member/login.fdl
+- 로그아웃 할 수 있다.
+	member/logout.fdl
+- 자신의 정보를 확인 할 수 있다.
+	member/show.fdl
+- 자신의 정보를 갱신 할 수 있다.
+	member/edit_form.fdl
+	member/update.fdl
+- 탈퇴 할 수 있다.
+	member/delete.fdl
+ */
+
 @Controller
 @RequestMapping("/member")
 public class MemberController {
