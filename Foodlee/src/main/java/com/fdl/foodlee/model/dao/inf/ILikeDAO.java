696d5ca1998c65dc3ffbe53d8160a9c0996829cd
compass-121 <58839593+compass-121@users.noreply.github.com>
2020-05-08 14:20:27 +0900

    d

--- Foodlee/src/main/java/com/fdl/foodlee/model/dao/inf/ILikeDAO.java
+++ Foodlee/src/main/java/com/fdl/foodlee/model/dao/inf/ILikeDAO.java
@@ -8,15 +8,15 @@ public class ILikeDAO {
 	// 좋아요 한 것을 리스트로 나열 할 수 있다.
 	List<LikeVO> showLikeList;
 	// 좋아요 한 메뉴 사진을 불러올 수 있다.
-	boolean showLikePic(LikeVO likeMenuPic);
+	boolean showLikePic;
 	// 좋아요 한 메뉴의 푸드트럭 이름을 불러올 수 있다.
-	boolean showLikeFtN();
+	boolean showLikeFtN;
 	// 좋아요 한 메뉴 이름을 불러올 수 있다.
-	boolean showLikeMenuN
+	boolean showLikeMenuN;
 	// 좋아요 한 메뉴 가격을 불러올 수 있다.
 	
 	// 좋아요 한 것을 취소(삭제) 할 수 있다.
-	boolean deleteLike();
+	boolean deleteLike;
 	
 	// 좋아요 
 }
