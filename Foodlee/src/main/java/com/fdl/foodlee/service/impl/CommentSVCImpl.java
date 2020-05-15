898e52663fc6e3f075c1013417f32d1b7993cbed
compass-121 <58839593+compass-121@users.noreply.github.com>
2020-05-15 18:45:12 +0900

    은경

--- Foodlee/src/main/java/com/fdl/foodlee/service/impl/CommentSVCImpl.java
+++ Foodlee/src/main/java/com/fdl/foodlee/service/impl/CommentSVCImpl.java
@@ -1,7 +1,30 @@
 package com.fdl.foodlee.service.impl;
 
+import java.util.List;
+
+import com.fdl.foodlee.model.vo.CommentVO;
 import com.fdl.foodlee.service.inf.ICommentSVC;
 
 public class CommentSVCImpl implements ICommentSVC {
+
+	@Override
+	public List<CommentVO> getCommentList(int commentNum) {
+		return null;
+	}
+
+	@Override
+	public int saveComment(CommentVO commentVO) {
+		return 0;
+	}
+
+	@Override
+	public int updateComment(CommentVO commentVO) {
+		return 0;
+	}
+
+	@Override
+	public int deleteComment(int commentId) {
+		return 0;
+	}
 
 }
--- Foodlee/src/main/java/com/fdl/foodlee/service/inf/ICommentSVC.java
+++ Foodlee/src/main/java/com/fdl/foodlee/service/inf/ICommentSVC.java
@@ -1,5 +1,17 @@
 package com.fdl.foodlee.service.inf;
 
-public interface ICommentSVC {
+import java.util.List;
 
+import com.fdl.foodlee.model.vo.CommentVO;
+
+public interface ICommentSVC {
+	
+	List<CommentVO> getCommentList(int commentNum);
+	
+	int saveComment(CommentVO commentVO);
+	
+	int updateComment(CommentVO commentVO);
+	
+	int deleteComment(int commentId);
+	
 }
