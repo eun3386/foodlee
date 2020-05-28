72e7abacefe1bbd9c41e69163e07aff09bdc3b51
DeSKoA <tellme146@gmail.com>
2020-05-28 15:47:45 +0900

    주문 수정

--- Foodlee/src/main/java/com/fdl/foodlee/model/dao/impl/FoodtruckMysqlDAOImpl.java
+++ Foodlee/src/main/java/com/fdl/foodlee/model/dao/impl/FoodtruckMysqlDAOImpl.java
@@ -28,107 +28,99 @@ import com.fdl.foodlee.model.vo.SellerVO;
 
 @Repository("ftJdbc")
 public class FoodtruckMysqlDAOImpl implements IFoodtruckDAO {
-	
+
 	// SQL 정의부
-	private static final String SQL_SELECT_ALL_FOODTRUCK 
-	= "select * from foodtrucks"; 
-	private static final String	SQL_INSERT_FOODTRUCK 
-	= "insert into foodtrucks values(null,?,?,?,?,?,?,?,?,0,0,null,?,null)";
-	private static final String	SQL_SELECT_FOODTRUCK_SELLER_ID 
-	= "select * from foodtrucks where seller_id = ?";
-	private static final String SQL_SELECT_FOODTRUCK_NAME 
-	= "select * from foodtrucks where foodtruck_name = ?";
-	private static final String	SQL_SELECT_FOODTRUCK_GU_CODE 
-	= "select * from foodtrucks where foodtruck_gu_code = ?";
-	private static final String	SQL_UPDATE_FOODTRUCK 
-	= "update foodtrucks set foodtruck_img_path=?, foodtruck_name=?, "
-		+ "foodtruck_main_menu=?, foodtruck_location=?, foodtruck_muni=?, "
-		+ "foodtruck_gu_code=?,foodtruck_operation_hour=?, "
-		+ "seller_foodtruck_coordinate=?";
-	private static final String	SQL_DELETE_FOODTRUCK 
-	= "delete from foodtrucks where foodtruck_name = ?";
-	
+	private static final String SQL_SELECT_ALL_FOODTRUCK = "select * from foodtrucks";
+	private static final String SQL_INSERT_FOODTRUCK = "insert into foodtrucks values(null,?,?,?,?,?,?,?,?,0,0,null,?,null)";
+	private static final String SQL_SELECT_FOODTRUCK_SELLER_ID = "select * from foodtrucks where seller_id = ?";
+	private static final String SQL_SELECT_FOODTRUCK_NAME = "select * from foodtrucks where foodtruck_name = ?";
+	private static final String SQL_SELECT_FOODTRUCK_GU_CODE = "select * from foodtrucks where foodtruck_gu_code = ?";
+	private static final String SQL_UPDATE_FOODTRUCK = "update foodtrucks set foodtruck_img_path=?, foodtruck_name=?, "
+			+ "foodtruck_main_menu=?, foodtruck_location=?, foodtruck_muni=?, "
+			+ "foodtruck_gu_code=?,foodtruck_operation_hour=?, " + "seller_foodtruck_coordinate=?";
+	private static final String SQL_DELETE_FOODTRUCK = "delete from foodtrucks where foodtruck_name = ?";
+
 	// 의존관계 자동주입...
 	@Autowired
 	private JdbcTemplate jtem;
-	
+
 	// 판매자의 새 푸드트럭을 등록 할 수 있다.
 	@Override
 	public boolean insertNewFoodtruck(FoodtruckVO ft) {
-		int r = jtem.update(SQL_INSERT_FOODTRUCK,
-					ft.getSellerId(), ft.getFoodtruckImgPath(),
-					ft.getFoodtruckName(), ft.getFoodtruckMainMenu(), 
-					ft.getMenuCategory(), ft.getFoodtruckLocation(), 
-					ft.getFoodtruckMuni(), ft.getFoodtruckGuCode(), 
-					ft.getFoodtruckOperationHour(),	ft.getSellerFoodtruckCoordinate());
+		int r = jtem.update(SQL_INSERT_FOODTRUCK, ft.getSellerId(), ft.getFoodtruckImgPath(), ft.getFoodtruckName(),
+				ft.getFoodtruckMainMenu(), ft.getMenuCategory(), ft.getFoodtruckLocation(), ft.getFoodtruckMuni(),
+				ft.getFoodtruckGuCode(), ft.getFoodtruckOperationHour(), ft.getSellerFoodtruckCoordinate());
 		return r == 1;
 	}
-	
+
 	// 판매자가 자신의 푸드 트럭 정보를 업데이트 할 수 있다.
 	@Override
 	public boolean updateOneFoodtruck(FoodtruckVO ft) {
-		int r = jtem.update(SQL_UPDATE_FOODTRUCK,
-				ft.getFoodtruckImgPath(), ft.getFoodtruckName(),
-				ft.getFoodtruckMainMenu(), ft.getMenuCategory(),
-				ft.getFoodtruckLocation(), ft.getFoodtruckMuni(),
-				ft.getFoodtruckGuCode(), ft.getFoodtruckOperationHour(),
-				ft.getSellerFoodtruckCoordinate(), ft.getLocationUpdatedAt());
+		int r = jtem.update(SQL_UPDATE_FOODTRUCK, ft.getFoodtruckImgPath(), ft.getFoodtruckName(),
+				ft.getFoodtruckMainMenu(), ft.getMenuCategory(), ft.getFoodtruckLocation(), ft.getFoodtruckMuni(),
+				ft.getFoodtruckGuCode(), ft.getFoodtruckOperationHour(), ft.getSellerFoodtruckCoordinate(),
+				ft.getLocationUpdatedAt());
 		return r == 1;
 	}
-	
+
 	// 판매자가 등록된 자신의 푸드 트럭을 삭제 할 수 있다.
 	@Override
 	public boolean deleteOneFoodtruck(FoodtruckVO ft) {
-		int r = jtem.update(SQL_DELETE_FOODTRUCK,
-				ft.getFoodtruckName());
+		int r = jtem.update(SQL_DELETE_FOODTRUCK, ft.getFoodtruckName());
 		return r == 1;
 	}
 
 	// 판매자의 ID를 받아 푸드트럭 정보를 조회 할 수 있다.
 	@Override
 	public FoodtruckVO selectOneFoodtruck(int sellerId) {
-		return (FoodtruckVO) jtem.queryForObject(SQL_SELECT_FOODTRUCK_SELLER_ID, 
-				BeanPropertyRowMapper
-				.newInstance(FoodtruckVO.class), sellerId);
+		try {
+			return (FoodtruckVO) jtem.queryForObject(SQL_SELECT_FOODTRUCK_SELLER_ID,
+					BeanPropertyRowMapper.newInstance(FoodtruckVO.class), sellerId);
+		} catch (EmptyResultDataAccessException e) {
+			return null;
+		}
 	}
 
 	// 푸드트럭 이름을 받아 푸드트럭 정보를 조회 할 수 있다.
 	@Override
 	public FoodtruckVO selectOneFoodtruck(String foodtruckName) {
-		return (FoodtruckVO) jtem.queryForObject(SQL_SELECT_FOODTRUCK_NAME, 
-				BeanPropertyRowMapper
-				.newInstance(FoodtruckVO.class), foodtruckName);
+		try {
+			return (FoodtruckVO) jtem.queryForObject(SQL_SELECT_FOODTRUCK_NAME,
+					BeanPropertyRowMapper.newInstance(FoodtruckVO.class), foodtruckName);
+		} catch (EmptyResultDataAccessException e) {
+			return null;
+		}
 	}
+
 	// 구 코드를 받아 푸드트럭 정보를 조회 할 수 있다.
 	@Override
 	public FoodtruckVO selectOneFoodtruckWithGuCode(int guCode) {
 		try {
-		return jtem.queryForObject(SQL_SELECT_FOODTRUCK_GU_CODE, 
-				BeanPropertyRowMapper
-				.newInstance(FoodtruckVO.class), guCode);
-		} catch(EmptyResultDataAccessException e){
+			return jtem.queryForObject(SQL_SELECT_FOODTRUCK_GU_CODE,
+					BeanPropertyRowMapper.newInstance(FoodtruckVO.class), guCode);
+		} catch (EmptyResultDataAccessException e) {
 			System.out.println("등록된 구가 아닙니다.");
 			return null;
 		}
 	}
-	
+
 	// 구매자가 푸드 트럭을 좋아요 할 수 있다.
 	@Override
 	public boolean likeFoodtruck(int ftId, int mbId) {
 		return false;
 	}
+
 	// 구매자가 푸드 트럭 리스트를 검색 할 수 있다.
 	@Override
 	public List<FoodtruckVO> searchAllFoodtruck(String foodtruckMainMenu) {
 		return null;
 	}
-	
+
 	@Override
 	public List<FoodtruckVO> searchAllFoodtruckWithGuCode(int guCode) {
-		return jtem.query(SQL_SELECT_FOODTRUCK_GU_CODE, 
-				BeanPropertyRowMapper
-				.newInstance(FoodtruckVO.class), guCode);
+		return jtem.query(SQL_SELECT_FOODTRUCK_GU_CODE, BeanPropertyRowMapper.newInstance(FoodtruckVO.class), guCode);
 	}
+
 	// 등록된 푸드 트럭 리스트를 조회 할수 있다
 	@Override
 	public List<FoodtruckVO> searchAddAllFoodtruckList() {
@@ -136,15 +128,3 @@ public class FoodtruckMysqlDAOImpl implements IFoodtruckDAO {
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
